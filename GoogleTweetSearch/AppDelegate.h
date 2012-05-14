d1089847a55ab01468a34ebda6603fc928050d38
MAYU <mtsuzaka@rb3.so-net.ne.jp>
2012-05-15 04:15:05 +0900

    Initial Commit

---
+++ GoogleTweetSearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GoogleTweetSearch
+//
+//  Created by 真有 津坂 on 12/05/15.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GoogleTweetSearch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GoogleTweetSearch
+//
+//  Created by 真有 津坂 on 12/05/15.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
